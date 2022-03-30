9814b698b530db034d64544b6a3335b3e99cc8b0
globecen <julien.legrand.pro@gmail.com>
2022-03-30 09:32:17 +0200

    update

--- actions/searchAction.php
+++ actions/searchAction.php
@@ -6,5 +6,6 @@ $search_villeArrivee = htmlspecialchars($_POST['villeArrivee']);
 $search_dateDepart = htmlspecialchars($_POST['dateDepart']);
 $searchTrajet = $bdd->prepare('SELECT * FROM Trajet WHERE villeDepart = ? AND villeArrivee= ? AND dateDepart= ?');
 $searchTrajet->execute(array($search_villeDepart, $search_villeArrivee, $search_dateDepart));
-var_dump($searchTrajet);
+$routes = $searchTrajet->fetchAll();
+var_dump($routes);
 ?>
