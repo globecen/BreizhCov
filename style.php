2355d50853499f58989789fa494c2afcf78b810b
kikasu <hallkevindu29@gmail.com>
2022-03-30 15:35:57 +0200

    update

--- style.php
+++ style.php
@@ -1,11 +1,17 @@
 <style>
+
     html{
-        
-    }
-
-    body {
         background-color: #dfe9ff;
+        margin-bottom: 0px;
+        width: 50%;
     }
 
+     body{
+        background-image:
+  linear-gradient(to bottom, rgba(255,255,0,0.5), rgba(0,0,255,0.5)),
+  url('img/acceuil.png');
+        background-repeat: no-repeat;
+        height: 100%;
+    }
     
 </style>
