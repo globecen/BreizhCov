60d86c84d598fd01f18a25982089ac4e5bca8106
kikasu <hallkevindu29@gmail.com>
2022-03-30 14:51:36 +0200

    update

--- style.php
+++ style.php
@@ -1,12 +1,10 @@
 <style>
     html{
-        background-color: #dfe9ff;
+        
     }
 
     body {
-        background-image:
-  linear-gradient(to bottom, rgba(255,255,0,0.5), rgba(0,0,255,0.5)),
-  url('img/acceuil.png');
+        background-color: #dfe9ff;
     }
 
     
