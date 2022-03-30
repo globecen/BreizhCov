a8a089a61835b56fe951b8f3e353d090a67040ae
Ssonido <epalamour@gmail.com>
2022-03-30 10:51:21 +0200

    upd

--- actions/searchAction.php
+++ actions/searchAction.php
@@ -7,10 +7,21 @@ $search_dateDepart = htmlspecialchars($_POST['dateDepart']);
 $searchTrajet = $bdd->prepare('SELECT * FROM Trajet WHERE villeDepart = ? AND villeArrivee = ? AND dateDepart = ?');
 $searchTrajet->execute(array($search_villeDepart, $search_villeArrivee, $search_dateDepart));
 //$routes = $searchTrajet->fetchAll();
+<table class="table">
+  <thead>
+    <th scope="col">Départ</th>
+    <th scope="col">Arrivé</th>
+    <th scope="col">Date</th>
+    <th scope="col">Nombre de places</th>
+  </thead>
+  <tbody>  
 while($row = $searchTrajet->fetch()) {
-    echo $row['villeDepart'];
-    echo $row['villeArrive'];
-    echo $row['dateDepart'];
-    echo $row['places'];
+    <tr>
+        <td>echo $row['villeDepart'];</td>
+        <td>echo $row['villeArrivee'];</td>
+        <td>echo $row['dateDepart'];</td>
+        <td>echo $row['places'];</td>
+    </tr>
 }
+</table>
 ?>
