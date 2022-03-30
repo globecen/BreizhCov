cd380b19efa19b541723087cb93b73584aac5a3f
globecen <julien.legrand.pro@gmail.com>
2022-03-30 09:32:01 +0200

    update

--- mesTrajets.php
+++ mesTrajets.php
@@ -1,8 +1,12 @@
 <?php session_start();?>
 <?php include 'head.php'?>
-<?php require('database.php');?>
+
 <?php
 $user_id=$_SESSION['id'];
-$listUser = $bdd->prepare('SELECT * FROM Trajet WHERE idConducteur = '+user_id);
-//$listUser->execute());
+var_dump($user_id);
+$listUser = $bdd->query('SELECT * FROM Trajet WHERE idConducteur = '+user_id);
+$user = $stmt->fetch();
+var_dump($listUser);
+$listUser->execute());
 ?>
+<?php require('database.php');?>
