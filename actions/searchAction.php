081bb8974ab47b8b0b5d9679050d0ab0be4f71c0
Ssonido <epalamour@gmail.com>
2022-03-30 09:51:33 +0200

    upd

--- actions/searchAction.php
+++ actions/searchAction.php
@@ -3,9 +3,9 @@ session_start();
 require('database.php');
 $search_villeDepart = htmlspecialchars($_POST['villeDepart']);
 $search_villeArrivee = htmlspecialchars($_POST['villeArrivee']);
-$search_dateDepart = htmlspecialchars($_POST['dateDepart']);
-$searchTrajet = $bdd->prepare('SELECT * FROM Trajet WHERE villeDepart = ? AND villeArrivee= ? AND dateDepart= ?');
-$searchTrajet->execute(array($search_villeDepart, $search_villeArrivee, $search_dateDepart));
+//$search_dateDepart = htmlspecialchars($_POST['dateDepart']);
+$searchTrajet = $bdd->prepare('SELECT * FROM Trajet WHERE villeDepart = ? AND villeArrivee = ?');
+$searchTrajet->execute(array($search_villeDepart, $search_villeArrivee));
 $routes = $searchTrajet->fetchAll();
 var_dump($routes);
 ?>
