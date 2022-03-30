95b7e359a5c587d9205c2df587d073810da1eed6
Ssonido <epalamour@gmail.com>
2022-03-30 14:53:05 +0200

    upd

--- index.php
+++ index.php
@@ -7,21 +7,31 @@
 <html>
   <body>
     <br>
-    <form action="actions/searchAction.php" class="row row-cols-lg-auto g-3 align-items-center" method="POST">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+        </div>
+        <div class="col-sm">
+          <form action="actions/searchAction.php" class="row row-cols-lg-auto g-3 align-items-center" method="POST">
 
-    <?php if(isset($errorMsg)){echo '<p>'.$errorMsg.'</p>';}?>
-    <div class="form-row align-items-center">
-        <input type="text" placeholder="Ville de départ" class="form-control" name="villeDepart">
-    </div>
+          <?php if(isset($errorMsg)){echo '<p>'.$errorMsg.'</p>';}?>
+          <div class="form-row align-items-center">
+              <input type="text" placeholder="Ville de départ" class="form-control" name="villeDepart">
+          </div>
 
-    <div class="form-row align-items-center">
-        <input type="text " placeholder="Ville de départ" class="form-control" name="villeArrivee">
-    </div>
-    <div class="form-row align-items-center">
-    <input id="startDate" class="form-control" type="date" name="dateDepart"/>
-    </div>
-    <div class="form-row align-items-center">
-    <input type="submit" value="Rechercher" class="btn btn-lg btn-primary">
+          <div class="form-row align-items-center">
+              <input type="text " placeholder="Ville de départ" class="form-control" name="villeArrivee">
+          </div>
+          <div class="form-row align-items-center">
+            <input id="startDate" class="form-control" type="date" name="dateDepart"/>
+          </div>
+          <div class="form-row align-items-center">
+            <input type="submit" value="Rechercher" class="btn btn-lg btn-primary">
+          </div>
+        </div>
+        <div class="col-sm">
+        </div>
+      </div>
     </div>
   </body>
 </html>
