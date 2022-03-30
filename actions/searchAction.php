b8f7ad89da677f635f5b235a4ffb1c4d2955dc2d
globecen <julien.legrand.pro@gmail.com>
2022-03-30 11:48:55 +0200

    update

--- actions/searchAction.php
+++ actions/searchAction.php
@@ -1,3 +1,52 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="../bootstrap/css/bootstrap.css" rel="stylesheet">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="../boostrap/js/bootstrap.js"></script>
+    <title>BreizhCov</title>
+    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../index.php">BreizhCov</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+      </li>
+        <?php if ($_SESSION['auth'] == true){?>
+            <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../routes.php">Ajouter un trajet</a>
+        </li>
+          <?php }
+        else{?>
+           <li class="nav-item">
+          <a class="nav-link disabled" aria-current="page" href="../routes.php">Ajouter un trajet</a>
+        </li>
+          <?php }?>
+          <?php if ($_SESSION['auth'] == true){?>
+            <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../mesTrajets.php">Mes trajets</a>
+        </li>
+          <?php }?>
+        <?php if ($_SESSION['auth'] == false){?>
+            <li class="nav-item">
+            <a class="nav-link btn btn-default" href="../login.php">Connexion</a>
+          </li>
+          <?php }
+        else{?>
+            <li class="nav-item">
+            <a class="nav-link" href="../logoutAction.php">Deconnexion</a>
+          </li>
+          <?php }?>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+</head>
 <?php
 session_start();
 
