846ecc12646f17aef0da266bcfdfa47c3ff9c86d
Ssonido <epalamour@gmail.com>
2022-03-30 15:06:05 +0200

    upd

--- routes.php
+++ routes.php
@@ -4,22 +4,33 @@
 <!DOCTYPE html>
 <html>
   <body>
-    <form class="container" method="POST">
-
-    <?php if(isset($errorMsg)){echo '<p>'.$errorMsg.'</p>';}?>
     <br>
-    <div class="mb-3">
-        <input type="text" placeholder="Ville de départ" class="form-control" name="villeDepart">
-    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+        </div>
+        <div class="col-9">
+          <form class="container" method="POST">
 
-    <div class="mb-3">
-        <input type="text" placeholder="Ville d'arrivée" class="form-control" name="villeArrivee">
+          <?php if(isset($errorMsg)){echo '<p>'.$errorMsg.'</p>';}?>
+          <br>
+          <div class="mb-3">
+              <input type="text" placeholder="Ville de départ" class="form-control" name="villeDepart">
+          </div>
+
+          <div class="mb-3">
+              <input type="text" placeholder="Ville d'arrivée" class="form-control" name="villeArrivee">
+          </div>
+          <div class="mb-3">
+              <input type="text" placeholder="Nombre de places" class="form-control" name="places">
+          </div>
+          <input id="startDate" class="form-control" name="dateDepart" type="date" />
+          <br>
+          <button type="submit" class="btn btn-outline-dark" name="validate">Ajouter</button>
+        </div>
+        <div class="col">
+        </div>
+      </div>
     </div>
-    <div class="mb-3">
-        <input type="text" placeholder="Nombre de places" class="form-control" name="places">
-    </div>
-    <input id="startDate" class="form-control" name="dateDepart" type="date" />
-    <br>
-    <button type="submit" class="btn btn-outline-dark" name="validate">Ajouter</button>
   </body>
 </html>
--- signup.php
+++ signup.php
@@ -4,27 +4,37 @@
 <?php include 'head.php'?>
 <?php include 'style.php'?>
 <body>
-    <br><br>
-    <form class="container" method="POST">
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            </div>
+            <div class="col-9">
+                <form class="container" method="POST">
 
-        <?php if(isset($errorMsg)){echo '<p>'.$errorMsg.'</p>';}?>
+                    <?php if(isset($errorMsg)){echo '<p>'.$errorMsg.'</p>';}?>
 
-        <div class="mb-3">
-            <input type="text" class="form-control" placeholder="Nom" name="nom">
-        </div>
-        <div class="mb-3">
-            <input type="text" class="form-control" placeholder="Prénom" name="prenom">
-        </div>
-        <div class="mb-3">
-            <input type="password" class="form-control" placeholder="Mot de passe" name="motDePasse">
-        </div>
-        <div class="mb-3">
-            <input type="text" class="form-control" placeholder="Mail" name="mail">
-        </div>
+                    <div class="mb-3">
+                        <input type="text" class="form-control" placeholder="Nom" name="nom">
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" class="form-control" placeholder="Prénom" name="prenom">
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control" placeholder="Mot de passe" name="motDePasse">
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" class="form-control" placeholder="Mail" name="mail">
+                    </div>
 
-        <button type="submit" class="btn btn-lg btn-primary" name="validate">S'inscrire</button>
-        <br><br>
-        <a href="login.php"><p class="btn btn-lg btn-primary" >J'ai déjà un compte, je me connecte</p></a>
-    </form>
+                    <button type="submit" class="btn btn-lg btn-primary" name="validate">S'inscrire</button>
+                    <br><br>
+                    <a href="login.php"><p class="btn btn-lg btn-primary" >J'ai déjà un compte, je me connecte</p></a>
+                </form>
+            </div>
+            <div class="col">
+            </div>
+        </div>
+    </div>
 </body>
 </html>
