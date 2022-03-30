cb966df4bee5bd3bd437415c53128c4f47fc2fe2
kikasu <hallkevindu29@gmail.com>
2022-03-30 16:08:10 +0200

    update

--- actions/searchAction.php
+++ actions/searchAction.php
@@ -61,8 +61,7 @@ $searchTrajet->execute(array($search_villeDepart, $search_villeArrivee, $search_
 
 echo'<body style=
 "background-image: linear-gradient(to bottom, rgba(255,255,0,0.5), rgba(0,0,255,0.5);
-background-repeat: no-repeat;
-background-size: cover;">';
+">';
 echo'<div class="container">';
   echo'<div class="row">';
     echo'<div class="col">';
