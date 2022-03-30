14f8c33115851897b1ef7e35c0ac860400b62165
kikasu <hallkevindu29@gmail.com>
2022-03-30 16:03:07 +0200

    update

--- actions/searchAction.php
+++ actions/searchAction.php
@@ -1,54 +1,52 @@
 <?php include 'style.php'?>
 <head>
-  <body>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <!-- CSS only -->
-      <?php include 'style.php' ?>
-      <link href="../bootstrap/css/bootstrap.css" rel="stylesheet">
-      <!-- JavaScript Bundle with Popper -->
-      <script src="../boostrap/js/bootstrap.js"></script>
-      <title>BreizhCov</title>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="../index.php">BreizhCov</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-        </li>
-          <?php if ($_SESSION['auth'] == true){?>
-              <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="../routes.php">Ajouter un trajet</a>
-          </li>
-            <?php }
-          else{?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <?php include 'style.php' ?>
+    <link href="../bootstrap/css/bootstrap.css" rel="stylesheet">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="../boostrap/js/bootstrap.js"></script>
+    <title>BreizhCov</title>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../index.php">BreizhCov</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+      </li>
+        <?php if ($_SESSION['auth'] == true){?>
             <li class="nav-item">
-            <a class="nav-link disabled" aria-current="page" href="../routes.php">Ajouter un trajet</a>
+          <a class="nav-link active" aria-current="page" href="../routes.php">Ajouter un trajet</a>
+        </li>
+          <?php }
+        else{?>
+           <li class="nav-item">
+          <a class="nav-link disabled" aria-current="page" href="../routes.php">Ajouter un trajet</a>
+        </li>
+          <?php }?>
+          <?php if ($_SESSION['auth'] == true){?>
+            <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../mesTrajets.php">Mes trajets</a>
+        </li>
+          <?php }?>
+        <?php if ($_SESSION['auth'] == false){?>
+            <li class="nav-item">
+            <a class="nav-link btn btn-default" href="../login.php">Connexion</a>
           </li>
-            <?php }?>
-            <?php if ($_SESSION['auth'] == true){?>
-              <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="../mesTrajets.php">Mes trajets</a>
+          <?php }
+        else{?>
+            <li class="nav-item">
+            <a class="nav-link" href="../logoutAction.php">Deconnexion</a>
           </li>
-            <?php }?>
-          <?php if ($_SESSION['auth'] == false){?>
-              <li class="nav-item">
-              <a class="nav-link btn btn-default" href="../login.php">Connexion</a>
-            </li>
-            <?php }
-          else{?>
-              <li class="nav-item">
-              <a class="nav-link" href="../logoutAction.php">Deconnexion</a>
-            </li>
-            <?php }?>
-          
-          </ul>
-        </div>
-      </div> 
-  </body>
+          <?php }?>
+        
+      </ul>
+    </div>
+  </div>
 </nav>
 </head>
 <?php
@@ -61,6 +59,9 @@ $search_dateDepart = htmlspecialchars($_POST['dateDepart']);
 $searchTrajet = $bdd->prepare('SELECT * FROM Trajet WHERE villeDepart = ? AND villeArrivee = ? AND dateDepart = ?');
 $searchTrajet->execute(array($search_villeDepart, $search_villeArrivee, $search_dateDepart));
 
+echo'<body style="background-image: linear-gradient(to bottom, rgba(255,255,0,0.5), rgba(0,0,255,0.5)),url('img/acceuil.png');
+background-repeat: no-repeat;
+background-size: cover;">';
 echo'<div class="container">';
   echo'<div class="row">';
     echo'<div class="col">';
@@ -85,4 +86,5 @@ echo'<div class="container">';
     echo'</div>';
   echo'</div>';
 echo'</div>';
+echo'</body>';
 ?>
